6dd20ad0c5fb700cc9ed2392b96d6d069d5ffffb
hardySin <deep16hardy@gmail.com>
2021-08-25 01:06:07 +0530

    comment on socket provider class

--- src/userPanel/helper/socketProvider.js
+++ src/userPanel/helper/socketProvider.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SocketHelper from '../helper/SocketHelper';
 
+// always need to check out the  create context till understand properly
 const SocketContext = React.createContext(SocketHelper.getSocket());
 
 export default SocketContext;
